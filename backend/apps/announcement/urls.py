399ccc4dd67fe89a2b052da686e7937a4144071d
kimyounghwan7 <peww99@gmail.com>
2025-04-13 15:49:57 +0900

    202504131549_bill_announcement_urls_fix

--- backend/apps/announcement/urls.py
+++ backend/apps/announcement/urls.py
@@ -1,4 +1,7 @@
-# from django.urls import path
-# from .views import *
+from django.urls import path
+from .views import announcement_list, announcement_detail
 
-# urlpatterns = []
+urlpatterns = [
+	path('announcements/', announcement_list, name='announcement-list'),
+	path('announcements/<int:id>', announcement_detail, name='announcement-detail'),
+]
